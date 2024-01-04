93527b7f22ca573ad727d7f28be4f934d1361621
dhruv <dhruv@gmail.com>
2024-01-05 03:16:57 +0530

    home layout working

--- LMSproject/src/App.jsx
+++ LMSproject/src/App.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
 import './App.css'
-import { Routes } from 'react-router-dom'
+// import { Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './Components/Footer'
+import HomeLayout from './Layouts/HomeLayout'
+import HomePage from './Pages/HomePage'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-  
-  <Footer/>
-  
+   
+    
+  <Routes>
+
+<Route path='/' element={<HomePage />} > </Route>
+
+  </Routes>  
+ 
   </>
   )
 }
